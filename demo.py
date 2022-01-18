b9fea52c85f58f54c08c32c0edfc7fc773288614
Manoj <manojk.yenugula@gmail.com>
2022-01-18 12:27:48 +0530

    Basics

---
+++ demo.py
@@ -0,0 +1,23 @@
+# #For doing math calculation we need to add "import math"
+# print(max(12,33,432,4323,645333))
+
+# # All about indexing [start:stop:step]
+# #step = for reversing the string we have to use negative values
+
+# Company = "Tata Consultancy Service"
+# first_name = Company[0::2]
+# print(first_name)
+# #Slicing43
+# slice = slice(5,-8)
+# print(Company[slice]) #here the slice should be in a "Square Brakcet"
+
+#Conditional Statement
+marks = int(input("How many marks you get in physics: "))
+
+if(marks >= 35):
+    print("Very Lucky guy You got a pass marks with border marks")
+elif(marks < 35):
+    print("Better luck next time you got failed in this exam")
+else:
+    print("Please rewrite the exam")
+ 
